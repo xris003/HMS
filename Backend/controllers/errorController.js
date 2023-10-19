2f7fe440f403cd91313916cbedb669f91840a143
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2023-10-19 12:33:24 +0100

    duplicateandvalidationerror

--- Backend/controllers/errorController.js
+++ Backend/controllers/errorController.js
@@ -5,6 +5,21 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  console.log(value);
+
+  const message = `Duplicate field value: ${value} Please use another value`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid Input data. ${errors.join(". ")}`;
+  return new AppError(message, 404);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -46,8 +61,9 @@ module.exports = (err, req, res, next) => {
   } else if (process.env.NODE_ENV === "production") {
     let error = { ...err };
 
-    if (err.name === "CastError") error = handleCastErrorDB(error);
-
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (err.name === "ValidationError") error = handleValidationErrorDB(error);
     sendErrorProd(error, res);
   }
 };
