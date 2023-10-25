7806a8528de76a40bb1c76e3077b1bd201fd5e21
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2023-10-25 15:12:40 +0100

    security

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -8,13 +12,33 @@ const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
-// 1) Middlewares
+// 1) GLOBAL MIDDLEWARE
+// Set security HTTP headers
+app.use(helmet());
+
+// Development Logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(express.json());
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
 
+//Body parser, reading data from the body into req.body
+app.use(express.json({ limit: "10kb" }));
+
+// Data Sanitization against NoSQL Query Injection
+app.use(mongoSanitize());
+
+// Data Sanitize cross site scripting
+app.use(xss());
+
+// Serving static files
 app.use((req, res, next) => {
   console.log("Hello from the middleware 😃");
   next();
