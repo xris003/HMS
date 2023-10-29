f3de361fea045e82391998c7cac644aff3efbb96
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2023-10-30 00:47:46 +0100

    updateAuth

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -18,6 +18,8 @@ router.get("/me", userController.getMe, userController.getUser);
 router.patch("/updateMe", userController.updateMe);
 router.delete("/deleteMe", userController.deleteMe);
 
+router.use(authController.restrictTo("admin"));
+
 router
   .route("/")
   .get(userController.getAllUsers)
