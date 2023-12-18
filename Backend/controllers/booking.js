64657acf9d4866eb37c11d7cc5b68f2caac7c6ac
Chris-Ikaria Gabriel <chrisikariagabriel@gmail.com>
2023-12-18 23:49:14 +0100

    paystack

--- Backend/controllers/booking.js
+++ Backend/controllers/booking.js
@@ -38,3 +38,40 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     session,
   });
 });
+
+const https = require("https");
+
+const params = JSON.stringify({
+  email: "customer@email.com",
+  amount: "20000",
+});
+
+const options = {
+  hostname: "api.paystack.co",
+  port: 443,
+  path: "/transaction/initialize",
+  method: "POST",
+  headers: {
+    Authorization: "Bearer SECRET_KEY",
+    "Content-Type": "application/json",
+  },
+};
+
+const req = https
+  .request(options, (res) => {
+    let data = "";
+
+    res.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    res.on("end", () => {
+      console.log(JSON.parse(data));
+    });
+  })
+  .on("error", (error) => {
+    console.error(error);
+  });
+
+req.write(params);
+req.end();
