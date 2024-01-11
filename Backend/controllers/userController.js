1305dcf6a25d3ad0813527d1910a21ff0bb56eb0
xris003 <chrisikariagabriel@gmail.com>
2024-01-11 16:48:39 +0100

    userphoto

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,7 +1,23 @@
+const multer = require("multer");
 const AppError = require("../utils/appError");
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
 const factory = require("./../controllers/handleFactory");
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new AppError("Not an image! Please upload only images.", 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
 
 const filterObj = (obj, ...allowedFileds) => {
   const newObj = {};
